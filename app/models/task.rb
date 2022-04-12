59ceb5cf4f94593c565560b290b82ffacdbf0426
Ana Perola <anaperolinha@hotmail.com>
2022-04-12 13:23:12 -0300

    Add: news messages validates

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,10 @@ class Task < ApplicationRecord
   enum priority: {low: 0, medium: 10, high: 20}
   enum status: {incomplete: 0, complete: 10}
 
-  validates :title, length: {minimum: 4}
-  validates :title, length: {maximum: 20}
+  validates :title, length: {minimum: 4, message: 'Title too short, Minimum of 4 characters'}
+  validates :title, length: {maximum: 20, message: 'Title too long, Maximum of 20 characters'}
   validates :description, presence: true 
-  validates :description, length: {maximum: 280} 
+  validates :description, length: {maximum: 280, message: 'Description Max Length is 280 characters'} 
 
   belongs_to :user
   has_many :comments
