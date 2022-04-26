f0248ec5219dd23ba8b61ed0e747d613ba7fc9a1
Ana Perola <anaperolinha@hotmail.com>
2022-04-26 14:15:55 -0300

    Fix permission user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
     #
     user ||= User.new # guest user (not logged in)
     if user.admin?
-      can :manage, :all
+      can %i[read destroy update], :all
     else
       can :read, :all
     end
